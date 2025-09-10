7255dfc27377baa92a3a30a5d5467f1616a8e8f8
BaltiBlast <fougeray.florian@gmail.com>
2025-09-10 16:22:01 +0200

    Route & controller ok ✅

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const router = require("./router.js");
 
 const app = express();
 const PORT = 3000;
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`La broche tourne sur http://localhost:${PORT}`);
