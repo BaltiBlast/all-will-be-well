b90592d229eec7ca61f3c3adfef46bd4370eff73
BaltiBlast <fougeray.florian@gmail.com>
2025-09-13 19:02:22 +0200

    connection set with mongo DB ✅

---
+++ services/mongoDB.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const { DB_URI, NODE_ENV } = process.env;
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    if (NODE_ENV === "dev") {
+      console.log("✅ Connected to MongoDB with Mongoose !");
+    }
+  })
+  .catch((error) => {
+    console.error("❌ Connection to MongoDB failed:", error);
+    process.exit(1);
+  });
+
+module.exports = mongoose;
