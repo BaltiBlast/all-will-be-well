ed9f05d2f5f500323ec4673c2102c690a05d00e6
BaltiBlast <fougeray.florian@gmail.com>
2025-09-13 12:03:19 +0200

    add animated placeholder on form's textarea

--- public/js/ui.script.js
+++ public/js/ui.script.js
@@ -1,6 +1,44 @@
+import { placeholders } from "./utils.script.js";
+
 const ux = {
   init: () => {
-    console.log("INIT UX");
+    ux.animatePlaceholder();
+  },
+  // ================================================================================ //
+  // Textarea placeholder's animation
+  // ================================================================================ //
+  setPlaceholderText: (fakePlaceholder, text) => {
+    fakePlaceholder.textContent = text;
+  },
+
+  switchPlaceholder: (fakePlaceholder, placeholders, index) => {
+    fakePlaceholder.classList.add("out");
+
+    setTimeout(() => {
+      ux.setPlaceholderText(fakePlaceholder, placeholders[index]);
+      fakePlaceholder.classList.remove("out");
+    }, 500);
+  },
+
+  animatePlaceholder: () => {
+    const textarea = document.getElementById("message");
+    const fakePlaceholder = document.getElementById("fake-placeholder");
+    if (!textarea || !fakePlaceholder) return;
+
+    let index = 0;
+
+    ux.switchPlaceholder(fakePlaceholder, placeholders, index);
+
+    setInterval(() => {
+      if (!textarea.value) {
+        index = (index + 1) % placeholders.length;
+        ux.switchPlaceholder(fakePlaceholder, placeholders, index);
+      }
+    }, 4000);
+
+    textarea.addEventListener("input", () => {
+      fakePlaceholder.style.display = textarea.value ? "none" : "block";
+    });
   },
 };
 
