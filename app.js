0375b5ed5276c5326bbb4fb2118269839deb855d
BaltiBlast <fougeray.florian@gmail.com>
2025-09-16 14:21:12 +0200

    init session + flash for using temporary requeste message

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const router = require("./router.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const flashMessage = require("./middlewares/message.middleware.js");
 
 const app = express();
 const PORT = 3000;
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 10 * 60 * 1000 },
+  })
+);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(express.json({ limit: "1mb" }));
+app.use(flash());
+app.use(flashMessage);
 app.use(router);
 
 app.listen(PORT, () => {
